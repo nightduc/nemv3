76d33ec29502b83e64a2aca91794ded7d3cf9d57
changsoo kim <nightduc@yahoo.com>
2019-04-09 13:50:36 +0900

    모던웹(NEMV) 제작 18 api 설정하기

--- be/routes/api/index.js
+++ be/routes/api/index.js
@@ -1,9 +1,14 @@
+var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
 
+router.get('/hello', function(req, res, next) {
+  res.send({ msg:'hello', a: 1 });
+});
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.all('*', function(req, res, next) {
+  next(createError(404,'No API'));
 });
 
 module.exports = router;
